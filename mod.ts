daff0da23b8af2861aaa64e35be7a36ef5710843
Omochice <mochice.mls.ntl@gmail.com>
2023-08-09 01:30:23 +0900

    feat: add some get functions

---
+++ mod.ts
@@ -0,0 +1,50 @@
+import $ from "https://deno.land/x/dax@0.34.0/mod.ts";
+import { err, ok, Result } from "npm:neverthrow@6.0.0";
+import { $array } from "npm:lizod@0.2.6";
+import type { Display, Space, Window } from "./type.ts";
+import { isDisplay, isSpace, isWindow } from "./validator.ts";
+
+export async function getSpaces(): Promise<Result<Space[], Error>> {
+  try {
+    const stdout = await $`yabai --message query --spaces`.json();
+    if ($array(isSpace)(stdout)) {
+      return ok(stdout);
+    }
+    return err(new Error("error"));
+  } catch (e: unknown) {
+    if (e instanceof Error) {
+      return err(e);
+    }
+    return err(new Error("unknown error", { cause: e }));
+  }
+}
+
+export async function getDisplays(): Promise<Result<Display[], Error>> {
+  try {
+    const stdout = await $`yabai --message query --displays`.json();
+    if ($array(isDisplay)(stdout)) {
+      return ok(stdout);
+    }
+    return err(new Error("error"));
+  } catch (e: unknown) {
+    if (e instanceof Error) {
+      return err(e);
+    }
+    return err(new Error("unknown error", { cause: e }));
+  }
+}
+
+export async function getWindows(): Promise<Result<Window[], Error>> {
+  try {
+    const stdout = await $`yabai --message query --windows`.json();
+    if ($array(isWindow)(stdout)) {
+      return ok(stdout);
+    }
+    return err(new Error("error"));
+  } catch (e: unknown) {
+    if (e instanceof Error) {
+      return err(e);
+    }
+    return err(new Error("unknown error", { cause: e }));
+  }
+}
